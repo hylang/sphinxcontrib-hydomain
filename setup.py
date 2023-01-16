2a2dbc93b469903692572ec9ca0fa74497f0b7ed
Kodi Arfer <git@arfer.net>
2023-01-16 09:20:00 -0500

    Remove `python_requires` and Python v. classifiers
    Hy is a dependency and its `python_requires` is already carefully maintained. We don't need to mirror that here.

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
     description="Sphinx domain for documenting Hy code bases",
     long_description=readme(),
     zip_safe=False,
-    python_requires=">= 3.7, < 3.11",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
@@ -34,10 +33,6 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
         "Topic :: Documentation",
         "Topic :: Utilities",
     ],
